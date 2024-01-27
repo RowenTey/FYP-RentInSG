9f1ecd40037d4e47147a11c30dc203a4c38fb567
rowentey <kaiseong02@gmail.com>
2024-01-27 13:47:42 +0800

    feat: Implemented log rotation

--- scrape.sh
+++ scrape.sh
@@ -6,5 +6,21 @@ target_directory="/home/ec2-user/FYP-RentInSG"
 # Change to the specified directory
 cd "$target_directory" || { echo "Failed to change to directory: $target_directory"; exit 1; }
 
-# Run the Python file (replace "your_python_script.py" with your actual Python file)
-venv/bin/python 99co-scraper.py -d >> cron-job.log
+# Set log directory and file prefix
+log_directory="$target_directory/logs"
+log_prefix="cron-job"
+
+# Create log directory if it doesn't exist
+mkdir -p "$log_directory"
+
+# Get the current date and time
+current_date=$(date +"%Y-%m-%d_%H-%M-%S")
+
+# Set log file name with timestamp
+log_file="$log_directory/${log_prefix}_${current_date}.log"
+
+# Run the Python file and append output to the log file
+venv/bin/python 99co-scraper.py >> "$log_file" 2>&1
+
+# Optionally, remove old log files that are more than 7 days old
+find "$log_directory" -name "${log_prefix}_*.log" -mtime +7 -exec rm {} \;
