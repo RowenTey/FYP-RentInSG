f86bf9c04949a4510a3dc523cf4ee47e60c3de0d
rowentey <kaiseong02@gmail.com>
2024-02-22 15:14:44 +0800

    chore: Refactored s3_uploader

--- utils/upload_to_s3.py
+++ utils/upload_to_s3.py
@@ -67,11 +67,10 @@ def upload_files_to_s3(local_directory, bucket_name):
     # List all files in the local directory
     for root, _, files in os.walk(local_directory):
         for file in files:
-            print(file)
             local_file_path = os.path.join(root, file)
             # Enforcing the format rental_prices/ninety_nine/*.csv
             s3_file_path = os.path.join(
-                root, file).replace(os.path.sep, '/')[2:]
+                root, file).replace(os.path.sep, '/')[3:]
 
             # Calculate the hash of the local file
             file_hash = calculate_file_hash(local_file_path)
@@ -84,7 +83,7 @@ def upload_files_to_s3(local_directory, bucket_name):
 
                 # Upload the Parquet file to S3
                 s3_file_path = s3_file_path.replace('.csv', '.parquet')
-                s3.upload_file(parquet_file_path, bucket_name, s3_file_path)
+                # s3.upload_file(parquet_file_path, bucket_name, s3_file_path)
                 print(
                     f'File uploaded: {parquet_file_path} to s3://{bucket_name}/{s3_file_path}')
 
@@ -109,7 +108,7 @@ if __name__ == "__main__":
     local_directory = '../rental_prices/ninety_nine'
 
     # Specify the S3 bucket name
-    bucket_name = 'fyp-2024-bucket'
+    bucket_name = os.getenv('S3_BUCKET')
 
     # Call the function to upload files to S3
     upload_files_to_s3(local_directory, bucket_name)
